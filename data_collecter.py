491060ba8b4441f6f0b34c2ebed86a83dd02db65
hereisjayant <hereisjayant@gmail.com>
2020-09-08 00:23:26 +0530

    fixed a bug

--- data_collecter.py
+++ data_collecter.py
@@ -17,29 +17,31 @@ count = 0
 while(1):
     ret, img = cam.read()
     img = cv2.flip(img, -1)
-    gray = cv2.cvtColor(img, cv2.COLOR_BR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    face = face_cascade.detectMultiscale(
+    face = face_cascade.detectMultiScale(
         gray,
         1.2,
-        5
+        5,
+        minSize=(30, 30)   #minimum rectangle size ti be considered a face
         )
     
     for(x, y, w, h) in face:
         count = count+1
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,255.0), 2)
         
+        print('Capturing image no. '+str(count))
         #saving the image
         file_name = "data_sets/Employee."+str(face_id)+"."+str(count)+".png"
         cv2.imwrite(file_name, gray[y:y+h, x:x+w]) #gets the face in grayscale
         #showing the photo being taken
         cv2.imshow('Preview', img)
         
-        k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
-        if k == 27:
-            break
-        elif count >= 30: # Take 30 face sample and stop video
-            break
+    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
+    elif count >= 30: # Take 30 face sample and stop video
+        break
 
 #closing all windows
 print("Data Collected!")
